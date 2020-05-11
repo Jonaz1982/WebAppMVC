64c8909a9279f573991b9163194ebc9fc14a73f5
Johnatan Gonzalez Velez <jgonzalez@sistecredito.com>
2020-05-11 13:10:09 -0500

    Controlador desde la Url

--- app/librerias/Core.php
+++ app/librerias/Core.php
@@ -14,11 +14,31 @@ class Core{
 
     //Constructor
     public function __construct(){
+        // haciendo pruebas de url 
+        //print_r($this->getUrl());
         $url = $this->getUrl();
+
+        //Buscar en controladores  si el controlador existe!!
+        if (file_exists('.../app/controladores/'. ucwords($url[0]).'.php')) {
+            //si existe se setea como controlador por defecto
+            $this ->controladorActual = ucwords($url[0]);
+
+            //unset indice
+            unset($url[0]);
+        }
+        //requerir el controlador 
+        require_once '../app/controladores/'.$this->controladorActual.'.php';
+        $this->controladorActual = new $this->controladorActual;
     }
    
     public function getUrl(){
-        echo $_GET['url'];
+        //echo $_GET['url'];
+        if (isset($_GET['url'])){
+            $url = rtrim($_GET['url'],'/');
+            $url = filter_var($url,FILTER_SANITIZE_URL);
+            $url = explode('/',$url);
+            return $url;
+        }
     }
 
 }
