25d00d1fd6c5d0efb29c9d9494564adf93debd32
Johnatan Gonzalez Velez <jgonzalez@sistecredito.com>
2020-05-13 10:44:35 -0500

    Creando Autocarga de clases

---
+++ app/config/configurar.php
@@ -0,0 +1,7 @@
+<?php
+//Ruta de la aplicacion
+define('RUTA_APP', dirname(dirname(__FILE__)));
+//Rutade la url : http://localhost:82/WebAppMVC/
+define('RUTA_URL', '_URl_');
+
+define('NOMBRESITIO', '_NOMBRE_SITIO');
--- app/vistas/paginas/inicio.php
+++ app/vistas/paginas/inicio.php
@@ -1,2 +1,3 @@
 
 <h1><?php echo $datos['titulo']; ?></h1>
+
