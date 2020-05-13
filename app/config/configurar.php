a8054c831eb671319a2d8aa2df2cf792fa97c69e
Johnatan Gonzalez Velez <jgonzalez@sistecredito.com>
2020-05-13 13:51:22 -0500

    Adicionando encabezado y pie de pagina

--- app/config/configurar.php
+++ app/config/configurar.php
@@ -2,6 +2,6 @@
 //Ruta de la aplicacion
 define('RUTA_APP', dirname(dirname(__FILE__)));
 //Rutade la url : http://localhost:82/WebAppMVC/
-define('RUTA_URL', '_URl_');
+define('RUTA_URL', 'http://localhost:82/WebAppMVC');
 
 define('NOMBRESITIO', '_NOMBRE_SITIO');
