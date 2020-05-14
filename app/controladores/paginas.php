7615d211946b04abcb1c97a383c3969816d1b1fb
Johnatan Gonzalez Velez <jgonzalez@sistecredito.com>
2020-05-14 12:32:26 -0500

    depurando el framework con MVC para poderlo trabajar en un futuro en otro proyecto

--- app/controladores/paginas.php
+++ app/controladores/paginas.php
@@ -2,28 +2,13 @@
 class Paginas extends Controlador{
 
     public function __construct(){
-
-      $this->articuloModelo = $this->modelo('Articulo');
-
-       // echo 'Controlador paginas cargando';
     }
     public function index(){
-        $articulos = $this->articuloModelo->obtenerArticulo();
-
+        
         $datos=[
-            'titulo'=>'Bienvenidos a WebAppMVC',
-            'articulos' => $articulos
-        ];
+            'titulo'=>'Bienvenidos a WebAppMVC'
+               ];
 
       $this->vista('paginas/inicio',$datos);
     }
-
-    public function articulo(){
-       // $this->vista('paginas/articulo');
-    }
-    public function actualizar($num_registro)
-    {
-      echo $num_registro;
-    }
-    
-}
+ }
--- app/vistas/paginas/inicio.php
+++ app/vistas/paginas/inicio.php
@@ -1,10 +1,3 @@
 <?require RUTA_APP .'/vistas/inc/header.php';?>
 <h1><?php echo $datos['titulo']; ?></h1>
-<ul>
-    <?php foreach($dato['articulos']as $articulo):?>
-        <li>
-            <?php echo $articulo ->titulo; ?>
-        </li>
-    <?php endforeach; ?>
-</ul>
 <?require RUTA_APP.'/vistas/inc/footer.php';?>
